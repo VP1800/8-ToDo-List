b4036a68c7f9e66fa02a5028a2da28a1a8348d7d
VP1800 <vidyasagarpatil1800@gmail.com>
2024-07-24 18:04:21 +0530

    modified:   app.js 	modified:   views/list.ejs

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const  express = require("express");
 const bodyParser = require("body-parser");
 var app= express();
-app.set("views engine","ejs");
+app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
@@ -23,20 +23,30 @@ const todo3 = new item({
 const todo4 = new item({
     name:"Take some rest"
 });
- todo.save();
+//  todo.save();
 // todo2.save();
 // todo3.save();
 // todo4.save();
-app.get("/",async function(req,res){
-    item.find({},function(err,foundItems){
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("list",{dayej : foundItems});
-        }
-    });
-});
+// app.get("/",function(req,res){
+//     item.find({},function(err,foundItems){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             res.render("list",{dayej : foundItems});
+//         }
+//     });
+// });
+
+app.get("/", async function(req, res) {
+    try {
+      const foundItems = await item.find({}).exec();
+      res.render("list", { dayej: foundItems });
+    } catch (err) {
+      console.error("Error occurred while fetching items:", err.message, err.stack);
+      res.status(500).send(`Error occurred while fetching items: ${err.message}`);
+    }
+  });
 app.listen(3000,function(){
     console.log("server started");
 });
