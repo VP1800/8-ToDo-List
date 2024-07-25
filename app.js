983eb488d212484456d252a092e9346f9e03658f
VP1800 <vidyasagarpatil1800@gmail.com>
2024-07-25 11:06:59 +0530

    modified:   app.js 	modified:   views/list.ejs

--- app.js
+++ app.js
@@ -11,33 +11,20 @@ const trySchema = new mongoose.Schema({
     name:String
 });
 const item = mongoose.model("task",trySchema);
-const todo = new item({
-    name:"Create some videos"
-});
-const todo2 = new item({
-    name:"learn DSA"
-});
-const todo3 = new item({
-    name:"learn react"
-});
-const todo4 = new item({
-    name:"Take some rest"
-});
+const Task = mongoose.model("task",trySchema);
+// const todo = new item({
+//     name:"Create some videos"
+// });
+// const todo2 = new item({
+//     name:"learn DSA"
+// });
 //  todo.save();
 // todo2.save();
-// todo3.save();
-// todo4.save();
 // app.get("/",function(req,res){
 //     item.find({},function(err,foundItems){
-//         if(err){
-//             console.log(err);
-//         }
-//         else{
-//             res.render("list",{dayej : foundItems});
-//         }
-//     });
+//     res.render("list", { dayej: foundItems });
+//     })
 // });
-
 app.get("/", async function(req, res) {
     try {
       const foundItems = await item.find({}).exec();
@@ -46,7 +33,45 @@ app.get("/", async function(req, res) {
       console.error("Error occurred while fetching items:", err.message, err.stack);
       res.status(500).send(`Error occurred while fetching items: ${err.message}`);
     }
-  });
+});
+app.post("/", function(req, res) {
+    const itemName = req.body.ele1;
+    const todo = new item({
+        name:itemName
+    });
+    todo.save();
+    res.redirect("/");
+});
+app.post("/delete", function(req, res) {
+    const checked = req.body.checkbox1;
+    const task = Task.findByIdAndDelete(checked);
+    async function removeTask(checked) {
+        try {
+          const task = await Task.findByIdAndDelete(checked);
+          console.log('Task removed:', task);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      // Call the function
+      removeTask(checked);
+      res.redirect("/");
+    // try {
+    //     item.deleteOne({ _id: checked });
+    //     res.send(`Item with ID ${checked} has been removed`);
+    //   } catch (err) {
+    //     res.status(500).send(`Error: ${err}`);
+    //   }
+});
+// app.post("/delete", function(req, res) {
+//     const checked = req.body.checkbox1;
+//     item.findByIdAndRemove(checked, function(err) {
+//         if (!err) {
+//             console.log("deleted");
+//             res.redirect("/");
+//         }
+//     });
+// });
 app.listen(3000,function(){
     console.log("server started");
 });
